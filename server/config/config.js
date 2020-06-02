838683417a8b1dbc5261732e18dd62535cda81a6
Tomas <Tomas.charcos@t-systems.com>
2020-06-02 17:58:20 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:568zkhlCE8S3iRoD@cluster0-h6lh4.mongodb.net/cafe'
+    //urlDB = 'mongodb+srv://admin:568zkhlCE8S3iRoD@cluster0-h6lh4.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 };
 
 process.env.urlDB = urlDB;
