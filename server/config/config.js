e0721a10cd601896a10500aebc24dedd1bf9a66c
Tomas <Tomas.charcos@t-systems.com>
2020-06-02 16:36:24 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 // =============================
 process.env.PORT = process.env.PORT || 3000
+
+// =============================
+// Entorno
+// =============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// =============================
+// Base de datos
+// =============================
+
+let urlDB;
+if (process.env.NODE_ENV = 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://admin:568zkhlCE8S3iRoD@cluster0-h6lh4.mongodb.net/cafe'
+};
+
+process.env.urlDB = urlDB;
